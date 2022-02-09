5505d9906782d762307c941d9fed70f9f5ee9da1
Arjay Jean <Arjay@Arjays-Macbook-Pro.local>
2022-02-08 22:01:32 -0500

    Moved file into a new folder

--- rpsgame.py
+++ rpsgame.py
@@ -82,8 +82,6 @@ def cadence(*rps):
 # THE GAME FUNCTION
 def rps_game():
 
-
-
     # PLAYER'S POINTS
     p1_points = 0
     cpu_points = 0
@@ -122,11 +120,16 @@ def rps_game():
         
 
         # PLAYER'S WINNING OR A TIE PRESENTATION
-        def who_won(winner):
-            print(f'{players_choices}: {winner} Wins!')
+        def who_won(choice, winner, point):
+            print(f'{choice}: {winner} Wins!')
+            sum(point, 1)
+            delay(3)
+            clear()
 
-        def present_tie():
-            print(f'{players_choices}: It is a TIE!!')
+        def present_tie(tie):
+            print(f'{tie}: It is a TIE!!')
+            delay(3)
+            clear()
 
 
         # HOW PLAYER'S WIN OR IF THERE IS A TIE GAME
@@ -135,28 +138,30 @@ def rps_game():
         tie_game = ["S vs S", "P vs P", "R vs R"]
 
         # HOW PLAYER 1 WINS
-        for how_p1_won in how_p1_wins: 
-            if players_choices == how_p1_won:
-                who_won(P1)
-                p1_points += 1
-                delay(3)
-                clear()
+        # for how_p1_won in how_p1_wins: 
+        #     if players_choices == how_p1_won:
+        #         who_won(P1)
+        #         p1_points += 1
+        #         delay(3)
+        #         clear()
         
-        # HOW CPU WINS
-        for how_cpu_won in how_cpu_wins:
-            if players_choices == how_cpu_won:
-                who_won(CPU)
-                cpu_points += 1
-                delay(3)
-                clear()
+        # # HOW CPU WINS
+        # for how_cpu_won in how_cpu_wins:
+        #     if players_choices == how_cpu_won:
+        #         who_won(CPU)
+        #         cpu_points += 1
+        #         delay(3)
+        #         clear()
             
-        # WHEN A TIE OCCURS
-        for tie in tie_game:
-            if players_choices == tie:
-                present_tie()
-                delay(3)
-                clear()
-
+        # # WHEN A TIE OCCURS
+        # for tie in tie_game:
+        #     if players_choices == tie:
+        #         present_tie()
+        #         delay(3)
+        #         clear()
+        player1_won = [who_won(how_p1_won, P1, p1_points) for how_p1_won in how_p1_wins if players_choices == how_p1_won]
+        cpu_won = [who_won(how_cpu_won, CPU, cpu_points) for how_cpu_won in how_cpu_wins if players_choices == how_cpu_won]
+        tied_up = [present_tie(tie) for tie in tie_game if players_choices == tie]
 
         # CHAMPION ANNOUNCEMENT
         def champion(champ):
